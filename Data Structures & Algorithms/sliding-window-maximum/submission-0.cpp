301bb4a8439c1f34723cdc543cbd14e6391f11c8
Shehab Sayed <106034258+shehabsayed@users.noreply.github.com>
2026-06-27 19:30:53 +0300

    Add: sliding-window-maximum - submission-0

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n = nums.size();
+        priority_queue<pair<int, int>> pq;
+        vector<int> ans;
+        for(int i=0;i<k;i++){
+            pq.push({nums[i], i});
+        }
+        ans.push_back(pq.top().first);
+        for(int i=k;i<n;i++){
+            while(!pq.empty() && pq.top().second <= (i - k)){
+                pq.pop();
+            }
+            pq.push({nums[i], i});
+            ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
