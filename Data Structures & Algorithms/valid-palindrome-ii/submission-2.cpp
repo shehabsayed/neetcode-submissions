a446f232b09d15a9b4d530fce982f4c93b05f2db
Shehab Sayed <106034258+shehabsayed@users.noreply.github.com>
2026-07-10 17:52:48 +0300

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool solve(string s, int i, int j){
+        while(i < j){
+            if(s[i] != s[j])
+                return false;   
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while (i < j){
+            if(s[i] != s[j]){
+                return (solve(s, i+1, j) || solve(s, i, j-1));
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }   
+};
