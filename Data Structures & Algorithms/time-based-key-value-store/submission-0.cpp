5d4d60edee14ddb243fa17d4ed75796f4bc9cfab
Shehab Sayed <106034258+shehabsayed@users.noreply.github.com>
2026-07-24 16:53:06 +0300

    Add: time-based-key-value-store - submission-0

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-0.cpp
@@ -0,0 +1,33 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> mp;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+
+    string get(string key, int timestamp) {
+        string ans = "";
+        if(mp.find(key) == mp.end()) return ans;
+        auto &v = mp[key];
+        int low = 0, high = v.size()-1;
+        while(low <= high){
+            int mid = low + ((high - low) / 2);
+            if(v[mid].first <= timestamp){
+                ans = v[mid].second;
+                low = mid + 1;
+            }else high = mid - 1;
+        }
+        return ans;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
